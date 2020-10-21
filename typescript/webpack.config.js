ccc4a2ed21d5d2d6b07917d4f5f0f768cc5d1c0a
sonpham11235 <50816481+sonpham11235@users.noreply.github.com>
2020-10-21 09:52:53 +0700

    config for sass

--- typescript/webpack.config.js
+++ typescript/webpack.config.js
@@ -21,7 +21,18 @@ module.exports = {
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader', options: { importLoaders: 1 } }
         ]
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ]
   },
   output: {
